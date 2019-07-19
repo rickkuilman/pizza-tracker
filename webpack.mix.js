7eac104151d5b816fca97dd83ae7bef19eced100
Rick Kuilman <rickkuilman@gmail.com>
2019-07-19 23:24:09 +0200

    Implemented tailwindcss and purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +15,15 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('tailwind.config.js')],
+    }).purgeCss({
+        // enabled: true,
+        globs: [
+            path.join(__dirname, 'node_modules/simplemde/**/*.js'),
+        ],
+
+        extensions: ['html', 'js', 'php', 'vue']
+    });
