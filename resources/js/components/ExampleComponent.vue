3ea5f0172220d2ee9c6c2539cf56de2fbf04d838
Rick Kuilman <rickkuilman@gmail.com>
2019-07-19 22:32:29 +0200

    Set up example component to retrieve MyEvent through Pusher

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,18 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+        Hi!
     </div>
 </template>
 
 <script>
+
     export default {
         mounted() {
-            console.log('Component mounted.')
+
+            window.Echo.channel('my-channel').listen('MyEvent', function(data) {
+                alert(JSON.stringify(data));
+            });
+
         }
     }
 </script>
